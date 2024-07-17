91fe0a1db4a2d26d05bd55b4d594ab74501670c1
Moticob <97236123+Moticob@users.noreply.github.com>
2024-07-17 11:27:39 -0700

    Create Transform.hs

---
+++ Transform.hs
@@ -0,0 +1,9 @@
+-- Transform.hs
+module Transform (applyTransformations) where
+
+import Mesh (Mesh)
+
+applyTransformations :: Mesh -> Mesh
+applyTransformations mesh = 
+    -- Apply transformations to mesh
+    mesh
